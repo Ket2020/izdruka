1f188772fe87372d873325247268346b00a9aaff
katrin2020 <mickevicakatrina@inbox.lv>
2021-01-11 12:50:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#Komentārus var 
+#rakstīt vairākas 
+#rindas"""
+print("Sveika,pasaule!")
+print(2 * 2)
+print(2 * 2, 2 * 3, 2 * 4, "Pitons")
+print(f"ja sakaitīsim 5 ar 7, iegūsim {5+7}.")
+print("Sveika, "+"pasaule!")
+print("Katrīna "*5)
+print(365*24*60*60)
+print(0.1+0.2-0.3)
+
+print()
